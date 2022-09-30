24dbb9d0ccfaaee7bc87df1ae0766b54263f7825
nohyunha95 <nohyunha95@gmail.com>
2022-09-30 19:54:33 +0900

    TODO 등록 성공

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,8 +1,59 @@
+import { useState } from "react";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import { Button, InputGroup, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
+
 function AddTodo() {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const onTitleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  const onContentChange = (event) => {
+    setContent(event.target.value);
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const url = "/todo/register";
+    const data = {
+      mno: 1,
+      uno: 1,
+      title: title,
+      content: content,
+    };
+    axios.post(url, data);
+
+    setTitle("");
+    setContent("");
+  };
+
   return (
     <>
-      <input type="text" />
-      <button>Submit</button>
+      <h3>Add Todo</h3>
+      <form onSubmit={onSubmit}>
+        <InputGroup size="lg">
+          <Form.Control
+            onChange={onTitleChange}
+            value={title}
+            placeholder="오늘 할 일"
+          />
+        </InputGroup>
+
+        <FloatingLabel controlId="floatingTextarea2" label="">
+          <Form.Control
+            as="textarea"
+            onChange={onContentChange}
+            value={content}
+            style={{ height: "100px" }}
+          />
+        </FloatingLabel>
+        <Button type="submit" variant="primary">
+          Submit
+        </Button>
+      </form>
     </>
   );
 }
